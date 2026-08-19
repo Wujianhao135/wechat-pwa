10f995c7f8a8c97c5207828be7135d0f42ee717c
Wujianhao135 <154825254+Wujianhao135@users.noreply.github.com>
2026-08-19 17:27:38 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'wechat-pwa-v1';
+const APP_FILES = [
+    './',
+    './index.html',
+    './manifest.webmanifest',
+    './js/lucide.min.js',
+    './icons/app-icon.svg',
+    './icons/app-icon-180.png',
+    './icons/app-icon-192.png',
+    './icons/app-icon-512.png'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(APP_FILES);
+        }).then(function () {
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(cacheNames.map(function (cacheName) {
+                if (cacheName !== CACHE_NAME) {
+                    return caches.delete(cacheName);
+                }
+            }));
+        }).then(function () {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request).then(function (networkResponse) {
+                if (networkResponse && networkResponse.status === 200 && event.request.url.startsWith(self.location.origin)) {
+                    const responseCopy = networkResponse.clone();
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request, responseCopy);
+                    });
+                }
+                return networkResponse;
+            });
+        })
+    );
+});
